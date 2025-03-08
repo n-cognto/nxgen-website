b50736a54c86ef85af365fb885d260ecf0bc657b
Victor Owino <vo4685336@gmail.com>
2025-03-08 21:06:49 +0300

    Remove unused Like and Comment models from projects

--- projects/models.py
+++ projects/models.py
@@ -21,23 +21,3 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
-
-class Like(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="likes")
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('project', 'user')  # Prevent duplicate likes
-
-    def __str__(self):
-        return f"{self.user.username} liked {self.project.title}"
-
-class Comment(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="comments")
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Comment by {self.user.username} on {self.project.title}"
