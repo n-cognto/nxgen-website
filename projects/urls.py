9ea55dbf387b30491487357c77563ceadc3c6412
Victor Owino <vo4685336@gmail.com>
2025-03-08 21:06:57 +0300

    Update project URLs to use slugs and add delete route for projects

--- projects/urls.py
+++ projects/urls.py
@@ -5,8 +5,7 @@ app_name = 'projects'
 
 urlpatterns = [
     path('', views.project_home, name='project_home'),
-    path('<int:project_id>/', views.project_detail, name='project_detail'),
     path('add/', views.add_project, name='add_project'),
-    path('<int:project_id>/comment/', views.add_comment, name='add_comment'),
-    path('<int:project_id>/like/', views.like_project, name='like_project'),
+    path('<slug:slug>/', views.project_detail, name='project_detail'),
+    path('<slug:slug>/delete/', views.delete_project, name='delete_project'),  # New delete route
 ]
