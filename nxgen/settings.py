8f590e0e88d344ffab7c5d25a731010e1eb45e47
Victor Owino <vo4685336@gmail.com>
2025-03-08 11:27:17 +0300

    Add STATICFILES_DIRS setting to configure static file directory

--- nxgen/settings.py
+++ nxgen/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Ensure Django looks for static files
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
