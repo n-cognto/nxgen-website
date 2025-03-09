cbcd92e9620b8ea28b28627d1424538dcf968ddb
benard00118 <benardopeter4@gmail.com>
2025-03-09 09:35:18 +0300

    Initialise Leadership and Testimonial models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class LeadershipTeam(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    bio = models.TextField()
+    image = models.ImageField(upload_to='leadership/')
+    linkedin = models.URLField(blank=True, null=True)
+    twitter = models.URLField(blank=True, null=True)
+    whatsapp = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Testimonial(models.Model):
+    content = models.TextField()
+    author_name = models.CharField(max_length=100)
+    author_position = models.CharField(max_length=100)
+    author_image = models.ImageField(upload_to='testimonials/')
+
+    def __str__(self):
+        return self.author_name
